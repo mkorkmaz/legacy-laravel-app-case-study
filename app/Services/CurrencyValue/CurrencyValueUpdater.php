12ddece113fc9e7ac0157d7444770120a4cf3b2b
Emre Ertoprak <yertoprak@yukatech.com>
2023-08-03 04:55:48 +0300

    fix : currency_id is not updated before situation now it fixed

--- app/Services/CurrencyValue/CurrencyValueUpdater.php
+++ app/Services/CurrencyValue/CurrencyValueUpdater.php
@@ -3,6 +3,7 @@
 namespace App\Services\CurrencyValue;
 
 use App\Repositories\CurrencyValue\CurrencyValueRepository;
+use App\Repositories\Currency\CurrencyRepository;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Exceptions\CurrencyValue\CurrencyValueException;
@@ -16,14 +17,17 @@ class CurrencyValueUpdater
      */
     protected $currencyValueRepository;
 
+    protected $currencyRepository;
+
     /**
      * Undocumented function
      *
      * @param CurrencyValueRepository $currencyValueRepository
      */
-    public function __construct(CurrencyValueRepository $currencyValueRepository)
+    public function __construct(CurrencyValueRepository $currencyValueRepository,CurrencyRepository $currencyRepository)
     {
         $this->currencyValueRepository = $currencyValueRepository;
+        $this->currencyRepository = $currencyRepository;
     }
 
     /**
@@ -36,16 +40,19 @@ class CurrencyValueUpdater
     public function updateById(Request $request,$id)
     {
         $checkCurrencyValue = $this->currencyValueRepository->find($id);
-
         if (!$checkCurrencyValue) {
             throw new CurrencyValueException('Currency value not found');
         }
+        $currencyCode = $request->input('currency_code');
+        $currencyData = $this->currencyRepository->getCurrencyByCode($request,$currencyCode);
+        $decodedCurrencyData = json_decode($currencyData,true);
         $data = $request->all();
         $dateTimeValue = Carbon::now();
         $formattedDateTime = $dateTimeValue->format('Y-m-d H:i:s');
         $data['logged_at'] = $formattedDateTime;
         $data['created_at'] = $formattedDateTime;
         $data['updated_at'] = $formattedDateTime;
+        $data['currency_id'] = $decodedCurrencyData[0]['id'];
 
         return $this->currencyValueRepository->update($id,$data);
     }
